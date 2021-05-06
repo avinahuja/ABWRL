a56f2da9b2a04a24f7e91cf1aa0724140445d179
avinahuja <avin@ahuja.ws>
2021-05-06 16:22:52 +0530

    hjeklgrkjhegwqjdfkglhtjrhegwrjklhkyj

---
+++ chain.js
@@ -0,0 +1,16 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var options = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness : 0.09,
+            length : 10
+        }
+        this.chain = Constraint.create(options)
+        World.add(world,this.chain)
+    }
+    display(){
+        strokeWeight(12)
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+    }
+}
